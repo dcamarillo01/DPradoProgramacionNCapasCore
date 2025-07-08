94d969be367e0ff475d163978320560e0ec5560e
dcamarillo01 <dcamarillo01@hotmail.com>
2025-07-08 15:36:28 -0600

    BL.Colonia Terminado.

--- BL/Direccion.cs
+++ BL/Direccion.cs
@@ -17,5 +17,40 @@ namespace BL
 
         }
 
+        //public static ML.Result Add(ML.Usuario Usuario)
+        //{
+
+        //    ML.Result result = new ML.Result();
+
+        //    try
+        //    {
+        //        using (DL_EF.DPradoProgramacionNCapasEntities context = new DL_EF.DPradoProgramacionNCapasEntities())
+        //        {
+
+        //            int filasAfectadas = context.DireccionAdd(Usuario.Direccion.Calle, Usuario.Direccion.NumeroInterior, Usuario.Direccion.NumeroExterior, Usuario.Direccion.Colonia.IdColonia);
+
+        //            if (filasAfectadas > 0)
+        //            {
+        //                result.Correct = true;
+        //            }
+        //            else
+        //            {
+        //                result.Correct = false;
+        //                result.ErrorMessage = "Ocurrio un problema al guardar la direccion";
+        //            }
+
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result.Correct = false;
+        //        result.ErrorMessage = ex.Message;
+        //        result.Ex = ex;
+        //    }
+
+        //    return result;
+        //}
+
+
     }
 }
