e11671899571d1a15db0723d578c5bc326df7259
dcamarillo01 <dcamarillo01@hotmail.com>
2025-07-08 15:27:46 -0600

    BL.Municipio Terminado.

--- BL/Municipio.cs
+++ BL/Municipio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,49 @@ namespace BL
 
         }
 
+
+        public ML.Result GetMunicipioByIdEstado(int IdEstado)
+        {
+
+            ML.Result result = new ML.Result();
+            result.Objects = new List<object>();
+
+            try
+            {
+
+                var query = _context.Municipios.FromSqlRaw("MunicipioGetByIdEstado").ToList();
+
+                if (query != null)
+                {
+
+                    foreach (var item in query)
+                    {
+
+                        ML.Municipio municipio = new ML.Municipio();
+                        municipio.IdMunicipio = item.IdMunicipio;
+                        municipio.Nombre = item.Nombre;
+                        result.Objects.Add(municipio);
+
+                    }
+
+                    result.Correct = true;
+                }
+                else
+                {
+                    result.Correct = false;
+                    result.ErrorMessage = "Ocurrio un problema al obtener municipios";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = true;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+        }
 
     }
 }
