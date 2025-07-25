a854b4b6a0f990892f5e8da73d4efb4b1d670669
dcamarillo01 <dcamarillo01@hotmail.com>
2025-07-24 19:02:16 -0600

    Se implemento validacion mediante DataAnnotations.

--- ML/Empleado.cs
+++ ML/Empleado.cs
@@ -23,8 +23,11 @@ namespace ML
         [RegularExpression(@"^[a-z A-Z]+$", ErrorMessage = "Solo se aceptan letras")]
         public string? ApellidoMaterno { get; set; }
         public string? FechaNacimiento { get; set; }
+        [RegularExpression(@"[A-z]{4}[0-9]{6}[A-z0-9]{3}", ErrorMessage = "RFC Invalido")]
         public string? RFC { get; set; }
+        [RegularExpression(@"^\d+$", ErrorMessage = "Solo se aceptan numeros")]
         public string? NSS { get; set; }
+        [RegularExpression(@"^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$", ErrorMessage = "CURP es invalido")]
         public string? CURP { get; set; }
         public string? FechaIngreso { get; set; }
         public ML.Departamento? Departamento { get; set; }
